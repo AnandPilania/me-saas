425c06425be3f64d2a43a21c984a2aaf9d124a9f
DHN Chandan <php.chandan@gmail.com>
2022-07-31 00:08:20 +0600

    fix: :bug: Fix importing log in mongo provider

--- src/providers/mongo.provider.ts
+++ src/providers/mongo.provider.ts
@@ -1,15 +1,15 @@
 import mongoose from "mongoose";
 import config from "config";
-import logger from "./logger.provider";
+import { log } from "./";
 
 const mongo_url = config.get<string>("mongo_url");
 
 async function connectMongo(): Promise<void> {
 	try {
 		await mongoose.connect(mongo_url);
-		logger.info("Connected to MongoDB");
+		log.info("Connected to MongoDB");
 	} catch (err) {
-		logger.error(err);
+		log.error(err);
 		process.exit(1);
 	}
 }
