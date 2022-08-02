a496d32d384fb32f464fd4f6821ef9a32d89a319
DHN Chandan <php.chandan@gmail.com>
2022-08-02 07:27:12 +0600

    refactor: :wrench: Add private and public keys

--- config/default.ts
+++ config/default.ts
@@ -3,4 +3,6 @@ export default {
 	log_level: process.env.LOG_LEVEL || "info",
 	mongo_url: process.env.MONGO_URL,
 	jwt_secret: process.env.JWT_SECRET,
+	public_key: process.env.PUBLIC_KEY,
+	private_key: process.env.PRIVATE_KEY,
 };
