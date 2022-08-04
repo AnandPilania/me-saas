8ec746bc260cd42eadd858fd56385a8b1928763b
DHN Chandan <php.chandan@gmail.com>
2022-08-05 04:50:33 +0600

    refactor: :boom: Move to class based approach

--- src/app.ts
+++ src/app.ts
@@ -1,37 +1,72 @@
 import dotenv from "dotenv";
 dotenv.config(); // Must be loaded before other imports
-import express from "express";
+import express, { Application } from "express";
 import cors from "cors";
+import compression from "compression";
+import helmet from "helmet";
+import hpp from "hpp";
+import cookieParser from "cookie-parser";
 import config from "config";
 import passport from "passport";
-import { connectMongo, log } from "./providers";
+import log from "./providers/logger.provider";
+import connectMongo from "./providers/mongo.provider";
 import router from "./routes";
 import { errorHandler } from "./middlewares";
 import passportAuth from "./modules/auth/passport/auth.config";
 
-const app = express();
-app.use(express.json());
+class App {
+	public app: Application;
+	public port: number | string;
 
-// Enable CORS
-app.use(
-	cors({
-		origin: "*",
-	}),
-);
+	public constructor() {
+		this.app = express();
+		this.port = config.get<number>("port");
 
-// Initialize passport
-app.use(passport.initialize());
-passportAuth(passport);
+		this.initializeDatabaseConnections().catch((error) => console.error(error));
+		this.initializeMiddlewares();
+		this.initializeRoutes();
+		this.initializeErrorHandling();
+	}
 
-app.use(router);
+	public listen = (): void => {
+		this.app.listen(this.port, () => {
+			log.info(`=========================================`);
+			log.info(`Server started on port ${this.port}`);
+			log.info(`=========================================`);
+		});
+	};
 
-// Error handler. Must be placed at the end of the middleware chain.
-app.use(errorHandler);
+	private initializeDatabaseConnections = async (): Promise<void> => {
+		await connectMongo();
+	};
 
-const port = config.get<number>("port");
+	private initializeMiddlewares = (): void => {
+		this.app.use(express.json());
+		this.app.use(express.urlencoded({ extended: false }));
+		// Enable CORS
+		this.app.use(
+			cors({
+				origin: "*",
+			}),
+		);
+		this.app.use(hpp());
+		this.app.use(helmet());
+		this.app.use(compression());
+		this.app.use(cookieParser());
+		// Initialize passport
+		this.app.use(passport.initialize());
+		passportAuth(passport);
+	};
 
-app.listen(port, async () => {
-	log.info(`Server started on port ${port}`);
+	private initializeRoutes = (): void => {
+		this.app.use(router);
+	};
 
-	await connectMongo();
-});
+	private initializeErrorHandling = (): void => {
+		// Error handler. Must be placed at the end of the middleware chain.
+		this.app.use(errorHandler);
+	};
+}
+
+const app = new App();
+app.listen();
