5030d5e6381e0b0a9b04654c05ed63211d668fcf
DHN Chandan <php.chandan@gmail.com>
2022-08-06 06:54:50 +0600

    refactor: :label: Fix type for passport and remove unnecessary codes

--- src/modules/auth/passport/auth.config.ts
+++ src/modules/auth/passport/auth.config.ts
@@ -1,25 +1,11 @@
 import log from "@providers/logger.provider";
-import usersService from "@modules/users/user.services";
-import { User } from "@modules/users/user.model";
 import localStrategy from "./local.strategy";
 import jwtStrategy from "./jwt.strategy";
 import queryStrategy from "./query.strategy";
+import { PassportStatic } from "passport";
 
-export default function (passport: any): void {
+export default function (passport: PassportStatic): void {
 	log.info("[passport] auth config");
-
-	passport.serializeUser(function (user: User, done: any) {
-		log.info("[serialize]");
-		done(null, false);
-	});
-	passport.deserializeUser(function (id: string, done: any) {
-		log.info("[deserialize]", id);
-
-		const user = usersService.findUserById(id);
-		if (user) {
-			done(null, user);
-		}
-	});
 
 	// Implement strategies
 	passport.use("local", localStrategy);
