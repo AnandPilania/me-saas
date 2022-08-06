597ca19b38aa0e746a325773796b9e801bb4d53e
DHN Chandan <php.chandan@gmail.com>
2022-08-06 20:45:21 +0600

    refactor: :recycle: Add username in create user schema

--- src/modules/users/user.schema.ts
+++ src/modules/users/user.schema.ts
@@ -3,6 +3,7 @@ import { object, string, TypeOf } from "zod";
 export const createUserSchema = object({
 	body: object({
 		email: string({ required_error: "Email is required." }).email("Email is not valid."),
+		username: string({ required_error: "Username is required." }),
 		name: string({ required_error: "Name is required." }),
 		password: string({ required_error: "Password is required." }).min(6, "Password must be at least 6 characters."),
 		passwordConfirmation: string({ required_error: "Password confirmation is required." }),
