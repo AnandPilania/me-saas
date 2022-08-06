ca84842400b801274baf78de8299daad60676a7c
DHN Chandan <php.chandan@gmail.com>
2022-08-06 06:58:11 +0600

    feat: :boom: Implement jwt socket authentication

--- src/modules/auth/auth.socket.ts
+++ src/modules/auth/auth.socket.ts
@@ -1,10 +1,14 @@
 import { Socket } from "socket.io";
 import { ISocket } from "@common/types/socket.types";
 import log from "@providers/logger.provider";
+import authService from "@modules/auth/auth.services";
+import { IJwtPayload } from "@common/types/jwt.types";
 
 export class AuthSocket implements ISocket {
 	public handleConnection = (socket: Socket): void => {
 		log.info("[auth] Socket connected");
+		log.info("[socket] connection id: " + socket.id);
+		// log.info(socket.handshake)
 		socket.emit("auth_ping", { message: "Welcome to the auth socket" });
 
 		socket.on("client_ping", (data: string) => {
@@ -13,9 +17,25 @@ export class AuthSocket implements ISocket {
 		});
 	};
 
-	public middlewareImplementation = (socket: Socket, next: () => void): void => {
+	public middlewareImplementation = (socket: Socket, next: (e?: Error) => void): void => {
 		// Implement middleware for auth here
-		next();
+		const auth_header = socket.handshake.headers.authorization;
+
+		if (auth_header) {
+			const payload: IJwtPayload | undefined = authService.verifyToken(auth_header);
+
+			if (payload) {
+				next();
+			} else {
+				log.error("[socket] Not authorized");
+				next(new Error("Not authorized"));
+				socket.disconnect();
+			}
+		} else {
+			log.error("[socket] Authorization header not found");
+			next(new Error("Authorization header not found"));
+			socket.disconnect();
+		}
 	};
 }
 
